5b3210800f81d02f31fb2872b5e053d83e67098c
Ashok Babu Marthu <ashokmarthu999@gmail.com>
2025-02-09 00:36:56 +0530

    Add context and Error Boundary to the class components

--- Day5 - Class Components -  Section - 14/src/components/Users.js
+++ Day5 - Class Components -  Section - 14/src/components/Users.js
@@ -1,37 +1,36 @@
-import { useState } from 'react';
-import User from './User';
+import { Component, useState } from "react";
+import User from "./User";
 
-import classes from './Users.module.css';
+import classes from "./Users.module.css";
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+    };
+  }
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+  toggleUsers() {
+    this.setState({ showUsers: !this.state.showUsers });
+  }
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
-
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className={classes.users}>
+        <button onClick={() => this.toggleUsers()}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && (
+          <ul>
+            {this.props.users.map((user) => (
+              <User key={user.id} user={user} />
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Users;
